0e1fc9c90faad5d32223777dc93748670657b602
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-06-03 16:48:23 +0200

    add-api of the backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import PostList from "./pages/PostList";
 import DefaultLayout from "./layout/DefaultLayout";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:3000/post").then((res) => {
+      const response = res.data;
+      console.log(response);
+    });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
