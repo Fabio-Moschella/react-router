117f1d15536c42da4502b1b1e31e26ee9c0c8620
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-06-03 17:08:53 +0200

    fix api and move in the postList

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function PostList() {
+  useEffect(() => {
+    axios.get("http://localhost:3000/post").then((res) => {
+      const response = res.data;
+      console.log(response);
+    });
+  }, []);
   return <h1>Post list</h1>;
 }
 
