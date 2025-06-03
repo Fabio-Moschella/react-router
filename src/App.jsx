f531ce01bb2fa3169762279bec58acd88e052644
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-06-03 13:19:58 +0200

    import route in appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import PostList from "./pages/PostList";
+import DefaultLayout from "./layout/DefaultLayout";
 
-function App() {}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+        <Route path="AboutUs" element={<AboutUs />} />
+        <Route path="posts" element={<PostList />} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default App;
